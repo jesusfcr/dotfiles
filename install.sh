b25599c61482749b1f23d35fa22252b44b9a2db6
Jesús Carpintero <jesus.carpintero@gmail.com>
2025-03-08 19:37:55 +0100

    Add fzf

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt upgrade git htop tree
+sudo apt upgrade git htop tree fzf
 
 curl -L https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-x86_64-unknown-linux-gnu.tar.gz -s | sudo tar xzvf - --strip 1 -C /usr/local/bin --wildcards "*/delta"
 
