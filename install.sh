c232392015e59a5d63b804e2787e0bbf94a59ded
Jesús Carpintero <jesus.carpintero@gmail.com>
2025-03-08 19:29:22 +0100

    Install oh-my-zsh

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 sudo apt update
 
-sudo apt install git
+sudo apt upgrade git htop tree
 
 curl -L https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-x86_64-unknown-linux-gnu.tar.gz -s | sudo tar xzvf - --strip 1 -C /usr/local/bin --wildcards "*/delta"
+
+# Install oh-my-zsh without updating zsh
+if [ ! -d /home/jesus/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
