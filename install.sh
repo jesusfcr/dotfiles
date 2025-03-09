7c84010827b7b93019eb5959b8012eec2197468b
Jesús Carpintero <jesus.carpintero@gmail.com>
2025-03-09 13:53:40 +0100

    Add tmux with tpm

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 sudo apt update
 
-sudo apt upgrade -y git htop tree fzf zsh-autosuggestions zsh-syntax-highlighting
+sudo apt upgrade -y git htop tree \
+  fzf zsh-autosuggestions zsh-syntax-highlighting \
+  tmux
 
 curl -L https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-x86_64-unknown-linux-gnu.tar.gz -s | sudo tar xzvf - --strip 1 -C /usr/local/bin --wildcards "*/delta"
 
@@ -25,5 +27,10 @@ fi
 
 if [ ! -d "${ZSH_PLUGINS}/zsh-syntax-highlighting" ]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_PLUGINS}/zsh-syntax-highlighting"
+fi
+
+if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
+  mkdir -p "$HOME/.config/tmux/plugins"
+  git clone https://github.com/tmux-plugins/tpm.git "$HOME/.config/tmux/plugins/tpm"
 fi
 
